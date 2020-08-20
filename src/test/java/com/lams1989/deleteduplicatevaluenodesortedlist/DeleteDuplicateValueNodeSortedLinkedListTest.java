25c4ad9e9e72368a04c03f5f1301e7f7ab08dfa3
Luis Munoz <luisalejandromunozsierra@gmail.com>
2020-08-20 16:57:37 -0300

    test implementation add

--- src/test/java/com/lams1989/deleteduplicatevaluenodesortedlist/DeleteDuplicateValueNodeSortedLinkedListTest.java
+++ src/test/java/com/lams1989/deleteduplicatevaluenodesortedlist/DeleteDuplicateValueNodeSortedLinkedListTest.java
@@ -1,21 +1,33 @@
 package com.lams1989.deleteduplicatevaluenodesortedlist;
 
-
 import com.lams1989.deleteduplicatevaluenodesortedlist.DeleteDuplicateValueNodeSortedLinkedList.SinglyLinkedList;
 import com.lams1989.deleteduplicatevaluenodesortedlist.DeleteDuplicateValueNodeSortedLinkedList.SinglyLinkedListNode;
 
 import junit.framework.TestCase;
 
-public class DeleteDuplicateValueNodeSortedLinkedListTest extends TestCase{
+public class DeleteDuplicateValueNodeSortedLinkedListTest extends TestCase {
 
-		public void test1() {
-			SinglyLinkedList  llist = new SinglyLinkedList();
-			int[] llistItem = { 141, 302, 164, 164, 530, 474 };
+	public void test1() {
+		SinglyLinkedList llist = new SinglyLinkedList();
+		int[] llistItem = { 530, 474, 302, 164, 164, 141 };
 
-			for (int i = 0; i < llistItem.length; i++) {
-				SinglyLinkedListNode llist_head = DeleteDuplicateValueNodeSortedLinkedList.insertNodeAtHead(llist.head, llistItem[i]);
-				llist.head = llist_head;
-			}
-			
+		for (int i = 0; i < llistItem.length; i++) {
+			SinglyLinkedListNode llist_head = DeleteDuplicateValueNodeSortedLinkedList.insertNodeAtHead(llist.head,
+					llistItem[i]);
+			llist.head = llist_head;
 		}
+
+		DeleteDuplicateValueNodeSortedLinkedList.deleteNodeDuplicate(llist.head);
+
+		int[] llistexpected = { 141, 164, 302, 474, 530 };
+		int[] llistactual = new int[llistexpected.length];
+		for (int i = 0; i < llistexpected.length; i++) {
+			llistactual[i] = llist.head.data;
+			llist.head = llist.head.next;
+		}
+
+		for (int i = 0; i < llistactual.length; i++) {
+			assertEquals(llistexpected[i], llistactual[i]);
+		}
+	}
 }
